77dc7636502470c99b569073934c140f0cebbba1
João Victor <joaomarquesvictor22@hotmail.com>
2025-03-10 09:17:35 -0300

    Feita alteração de estilo na page equipamentos

--- src/components/Others/CardPatrimonio/index.jsx
+++ src/components/Others/CardPatrimonio/index.jsx
@@ -6,7 +6,25 @@ function CardPatrimonio ({ patrimonio, local, obs ,categoria, marca, modelo, pro
     return (
 
         <section className={styles.cardPatrimonio}>
-            <div className={styles.listagem}>
+            <div className={styles.listagem}> 
+                <div className={styles.patrimonios}>
+                    <div className={styles.descricao}>
+                        <h1 className={styles.texto}>Patrimonio:</h1>
+                        <h2 className={styles.textoCampo}>{patrimonio}</h2>
+                    </div>
+                    <div className={styles.descricao}>
+                        <h1 className={styles.texto}>Local:</h1>
+                        <h2 className={styles.textoCampo}>{local}</h2>
+                    </div>
+                    <div className={styles.descricao}>
+                        <h1 className={styles.texto}>Empresa:</h1>
+                        <h2 className={styles.textoCampo}>{empresa}</h2>
+                    </div>
+                    <div className={styles.descricao}>
+                        <h1 className={styles.texto}>OBS:</h1>
+                        <h2 className={styles.textoCampo}>{obs}</h2>
+                    </div>
+                </div>
                 <div className={styles.equipamentos}>
                     <div className={styles.descricao}>
                         <h1 className={styles.texto}>Categoria:</h1>
@@ -32,25 +50,7 @@ function CardPatrimonio ({ patrimonio, local, obs ,categoria, marca, modelo, pro
                         <h1 className={styles.texto}>Disco:</h1>
                         <h2 className={styles.textoCampo}>{disco}</h2>
                     </div>
-                </div>    
-                <div className={styles.patrimonios}>
-                    <div className={styles.descricao}>
-                        <h1 className={styles.texto}>Patrimonio:</h1>
-                        <h2 className={styles.textoCampo}>{patrimonio}</h2>
-                    </div>
-                    <div className={styles.descricao}>
-                        <h1 className={styles.texto}>Local:</h1>
-                        <h2 className={styles.textoCampo}>{local}</h2>
-                    </div>
-                    <div className={styles.descricao}>
-                        <h1 className={styles.texto}>Empresa:</h1>
-                        <h2 className={styles.textoCampo}>{empresa}</h2>
-                    </div>
-                    <div className={styles.descricao}>
-                        <h1 className={styles.texto}>OBS:</h1>
-                        <h2 className={styles.textoCampo}>{obs}</h2>
-                    </div>
-                </div>
+                </div>   
             </div>
                 <div className={styles.botoes}>
                     <Link className={styles.botao} state={state} to={rotaPatrimonio}>Editar patrimônio</Link>  
